aeb6c941d1fa868bf632504214f066cabcef3de9
Gavin Boulton <gavboulton@gmail.com>
2018-10-11 17:58:25 +0100

    Expose error handler middleware

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const accessControlHeaders = require('./lib/accessControlHeaders');
+const errorHandler = require('./lib/errorHandler');
 
 module.exports = {
   accessControlHeaders,
+  errorHandler,
 };
